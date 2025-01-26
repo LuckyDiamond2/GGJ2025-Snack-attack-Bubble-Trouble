6def993e9e35b7c157a98bcd33135e1261e440af
Korbinian <korbinianstaudacher416+ggj@gmail.com>
2025-01-26 14:07:13 +0100

    worked on the screen management, not finished

--- scripts/loose_screen.gd
+++ scripts/loose_screen.gd
@@ -10,6 +10,36 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+# speech bubble button
 func _on_play_again_button_pressed() -> void:
+	playAgain()
+
+# try again buton
+func _on_t_try_again_button_pressed() -> void:
+	playAgain()
+
+func playAgain() -> void:
 	get_tree().change_scene_to_file("res://scenes/game_screen.tscn")
 	print("pressed 'Play Again' from loose screen")
+
+
+# quit game button
+func _on_quit_game_button_pressed() -> void:
+	quitGame()
+
+# quit game bubble button
+func _on_quit_game_bubble_button_pressed() -> void:
+	quitGame()
+	
+func quitGame() -> void:
+	print("quit game from loose screen")
+	get_tree().quit()
+
+
+# menu bubble button
+func _on_menu_bubble_button_pressed() -> void:
+	toMenu()
+
+func toMenu() -> void:
+	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
+	print("pressed 'Menu' from loose screen")
